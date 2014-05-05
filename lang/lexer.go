2c40a02f1a850e5ada487d14200e6204b4f923c0
Victor van Poppelen <vvanpo@gmail.com>
2014-05-04 23:16:22 -0400

    State-function-based lexer, no lexing functions implemented yet

--- lang/lexer.go
+++ lang/lexer.go
@@ -1,100 +1,89 @@
 package main
 
 import (
-	"bufio"
 	"io"
+	"io/ioutil"
+	"strings"
 	"log"
 	"unicode"
 	"unicode/utf8"
 )
 
-type char struct {
-	line int
-	col  int
-	rune
-}
-
-type tokenType int
+type terminal int
 
 const (
-	tIndent tokenType = iota
+	tIndent terminal = iota
 	tDedent
-	tKeyword
-	tDelimiter
-	tOperator
 	tIdentifier
 	tLiteral
+	tReserved
+	tOperator
+	tDelimiter
 )
 
-type token struct {
-	typ   tokenType
-	value string
+var reserved = [...]string{
+	"const", "byte", "block", "func", "...", "->", "jump", "return", "if",
+}
+var operators = [...]string{
+	":", ":=", "=", "+", "-", "*", "/", "**", "(", ")",
+}
+var delimiters = [...]string{
+	",",
 }
 
+type token struct {
+	terminal
+	attr string
+	num int64
+}
+
+type stateFn func(*lexer) stateFn
+
 type lexer struct {
-	*log.Logger
-	*bufio.Reader
-	last        char
-	cur         string
+	input		string
+	tokens      chan token
 	indent      []int
 	indent_rune rune
-	tokens      chan token
+	start		int
+	pos			int
 }
 
 func lex(r io.Reader) chan token {
+	input, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal("Input error")
+	}
 	l := &lexer{
-		log.New(os.Stderr, log.Prefix(), log.Flags()),
-		bufio.NewReader(r),
-		last:   char{rune: '\n'},
+		input:	string(input),
 		tokens: make(chan token),
 		indent: []int{0},
 	}
 	go func() {
 		defer close(l.tokens)
-		for {
+		for state := lexIndent; state != nil; {
+			state = state(l)
 		}
 	}()
 	return l.tokens
 }
 
-func (l *lexer) logError(err error, c char) {
-	l.Panicf("Line #%d, Column #%d:\n\t%s\n", c.line, c.col, err)
-}
-
-func (l *lexer) next() (c char) {
-	r, s, err := l.ReadRune()
-	if err != nil {
-		l.logError(err, l.last)
-	}
-	c := char{
-		line: l.last.line,
-		col:  l.last.col + len(string(l.last.rune)),
-		rune: r,
-	}
-	if l.last.rune == '\n' {
-		c.line++
-		c.col = 1
-	}
+func (l *lexer) next() (r rune) {
+	r, s := utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += s
 	if r == '\ufffd' && s == 1 {
-		l.logError("Invalid input encoding", c)
+		log.Fatal("Invalid input encoding")
 	}
+	return
 }
 
-func (l *lexer) peek() (c char) {
-	c = l.next()
-	err := l.UnreadRune()
-	if err != nil {
-		l.logError(err, c)
-	}
+func (l *lexer) lexIndent() stateFn {
+
 }
 
-func (l *lexer) emit(typ tokenType, v string) {
-	l.tokens <- token{typ: typ, value: v}
+func (l *lexer) lexIdentifer() stateFn {
+
 }
 
-const keywords = []string{
-	"const", "byte", "block", "func ", "...", "->", "jump", "return", "if",
-}
-const operators_delimiters = []string{
-	":", ":=", "=", "+", "-", "*", "/", "**", ",", "(", ")",
+func (l *lexer) lexLiteral() stateFn {
+
 }
