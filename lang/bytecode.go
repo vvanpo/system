030aaf10adde80e84a8d66a8aa685188b2665b7b
Victor van Poppelen <vvanpo@gmail.com>
2014-05-22 13:14:12 -0400

    addSymbolDef and addWord

--- lang/bytecode.go
+++ lang/bytecode.go
@@ -4,11 +4,9 @@ import (
 	"bytes"
 )
 
-type bytecode byte
-
 // Bytecodes are ordered by expected rate of incidence to aid compression
 const (
-	bError     bytecode = iota
+	bError     byte = iota
 	bSymbolDef          // Namespace decided by nesting within bFunction declarations or bBlock* structures
 	bWord
 	bByte
@@ -69,25 +67,30 @@ type bytecodeFile struct {
 	symbol     []symbol     // this value are converted to block types
 	imported   []symbol
 	literal    []literal
+	code       []byte
+	currentNamespace  *symbol
 }
 
 func newBytecodeFile() (b *bytecodeFile) {
 	b = new(bytecodeFile)
 	b.identifier = []identifier{"_", "_sp", "_fp", "_ip", "_text", "_data"}
+	for _, i := range b.identifier {
+		b.addSymbol(i, 0, nil)
+	}
 	return
 }
 
-func (b *bytecodeFile) addIdentifier(ident string) *identifier {
+func (b *bytecodeFile) addIdentifier(ident identifier) *identifier {
 	for _, i := range b.identifier {
-		if i == identifier(ident) {
+		if i == ident {
 			return &i
 		}
 	}
-	b.identifier = append(b.identifier, identifier(ident))
+	b.identifier = append(b.identifier, ident)
 	return &b.identifier[len(b.identifier)-1]
 }
 
-func (b *bytecodeFile) addSymbol(ident string, address uint, parent *symbol) (s *symbol) {
+func (b *bytecodeFile) addSymbol(ident identifier, address uint, parent *symbol) (s *symbol) {
 	s = new(symbol)
 	s.identifier = b.addIdentifier(ident)
 	s.address = address
@@ -98,15 +101,7 @@ func (b *bytecodeFile) addSymbol(ident string, address uint, parent *symbol) (s 
 
 // Assumes big-endian ordering of words (most-significant word passed in as first argument)
 func (b *bytecodeFile) addWordLiteral(l ...uint) {
-	lit := []byte{}
-	for _, i := range l {
-		tmp := []byte{}
-		for i != 0 {
-			tmp = append([]byte{byte(0xff | i)}, tmp...)
-			i >>= 8
-		}
-		lit = append(lit, tmp...)
-	}
+	lit := convertBigEndian(l...)
 	for _, v := range b.literal {
 		if bytes.Equal(lit, v) {
 			return
@@ -124,3 +119,32 @@ func (b *bytecodeFile) addByteLiteral(l ...byte) {
 	}
 	b.literal = append(b.literal, l)
 }
+
+func (b *bytecodeFile) addWord(w uint) {
+	w = convertBigEndian(w)
+	for uint(len(w)) < b.wordLength / 8 {
+		w = append([]byte{0}, w...)
+	}
+	b.code = append(b.code, w...)
+}
+
+func (b *bytecodeFile) addSymbolDef(s string) {
+	i := uint(len(b.symbol))
+	b.currentNamespace = b.addSymbol(identifier(s), uint(len(b.code)), b.currentNamespace)
+	b.code = append(b.code, bSymbolDef)
+	b.addWord(i)
+}
+
+func convertBigEndian(n ...uint) []byte {
+	num := []byte{}
+	for _, b := range n {
+		tmp := []byte{}
+		for b != 0 {
+			tmp = append([]byte{byte(0xff | b)}, tmp...)
+			b >>= 8
+		}
+		num = append(num, tmp...)
+	}
+	return num
+}
+
