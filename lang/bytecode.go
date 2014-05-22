e6ab8a4bd0d2c53b5eeee504f66d09647e3b532c
Victor van Poppelen <vvanpo@gmail.com>
2014-05-22 11:32:09 -0400

    addLiteral methods added to bytecodeFile type

--- lang/bytecode.go
+++ lang/bytecode.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"bytes"
+)
+
 type bytecode byte
 
 // Bytecodes are ordered by expected rate of incidence to aid compression
@@ -46,3 +50,58 @@ const (
 //	bInstructionPointer		//	Current instruction special register, identifer "_ip"
 //	bTextPointer			//	Beginning of text segment special variable "_text"
 //	bDataPointer			//	Beginning of data/heap segment special variable "_data"
+
+type identifier string
+
+type symbol struct {
+	*identifier
+	address uint
+	parent  *symbol
+}
+
+// Literals are encoded as sequences of bytes, until the minimum word-length (bytecodeFile.wordLength) can be established
+// The byte slice uses big-endian ordering, which is also the representation used in the compiled bytecode file
+type literal []byte
+
+type bytecodeFile struct {
+	wordLength  uint         // Minimum word-length is decided on using the largest memory
+	identifier  []identifier // reference made in the compiled code.  Literals that exceed
+	symbol      []symbol     // this value are converted to block types
+	imported    []*identifier
+	literal     []literal
+	textPointer uint // Beginning of text segment special variable "_text"
+	dataPointer uint // Beginning of data/heap segment special variable "_data"
+}
+
+func newBytecodeFile() (b *bytecodeFile) {
+	b = new(bytecodeFile)
+}
+
+// Assumes big-endian ordering of words (most-significant word passed in as first argument)
+func (b *bytecodeFile) addWordLiteral(l ...uint) {
+	lit := []byte{}
+	for _, i := range l {
+		tmp := []byte{}
+		for i != 0 {
+			tmp = append([]byte{byte(0xff | i)}, tmp...)
+			i >>= 8
+		}
+		lit = append(lit, tmp...)
+	}
+	for _, v := range b.literal {
+		if bytes.Equal(lit, v) {
+			return
+		}
+	}
+	b.literal = append(b.literal, lit)
+}
+
+// Assumes big-endian ordering of bytes
+func (b *bytecodeFile) addByteLiteral(l ...byte) {
+	for _, v := range b.literal {
+		if bytes.Equal(l, v) {
+			return
+		}
+	}
+	b.literal = append(b.literal, l)
+}
--- lang/symbol.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type symbol map[string]struct {
-	size uint64
-	ref  uint64
-}
