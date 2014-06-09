fa00a9ba918fb1ba77bf8ac1a661e6e289d637d9
Victor van Poppelen <vvanpo@gmail.com>
2014-06-09 17:50:45 -0400

    statement list instead of 'literal' list

--- lang/bytelang/bytelang.go
+++ lang/bytelang/bytelang.go
@@ -1,24 +1,27 @@
 package main
 
+// Bytecode markers
 const (
-	bError    byte = iota
-	bBytes         // Bytes literal
-	bFunction      // Function literal
-	bByte          // Variable types
-	bWord
-	bBlock
-	bVariable // Statement types
-	bFunctionCall
-	bIf
-	bAssignment
-	bReturn
-	bLiteralPointer     // Pointer to literal
-	bLiteralValue       // Value-copy of literal
-	bReference          // Variable reference
+	bError byte = iota
+	bByte       // Byte-wide variable type
+	bWord       // Word-wide variable type
+	bBlock      // Compile-time-defined-length variabl type
+	// Statements:
+	bVariable     // Variable declaration
+	bFunctionCall // Function call statement
+	bIf           // If statement
+	bAssignment   // Assignment statement
+	bReturn       // Return statement
+	// Expressions:
+	bLiteral   // Literal value
+	bReference // Variable reference
+	bFunction  // Function definition
+	// Unary operators:
 	bAddressReferenceOp // Variable address reference operator
-	bDereferenceOp      // Address dereference operator
+	bDereferenceOp      // Variable dereference operator
 	bNotOp
-	bAddOp // Binary operators
+	// Binary operators:
+	bAddOp
 	bSubtractOp
 	bMultiplyOp
 	bDivideFloorOp
@@ -48,38 +51,94 @@ type bytelang struct {
 	wordLength int          // Bytes per word
 	identifier []identifier // Identifier list
 	namespace               // Namespace tree
-	literal    []literal    // List of literals
+	statement  []statement  // List of literals
+}
+
+func (b *bytelang) putWord(w uint) string {
+	s := make([]byte, b.wordLength)
+	for i := b.wordLength; i >= 0; i-- {
+		s[i] = byte(0xff | w)
+		w >>= 8
+	}
+	return string(s)
+}
+
+func (b *bytelang) statementIndex(l statement) uint {
+	for i := range b.statement {
+		if b.statement[i] == l {
+			return uint(i + 1)
+		}
+	}
+	return 0
 }
 
 type identifier string
 
 type namespace struct {
-	*variable
+	*variable   // Reverse index into statement list
 	*identifier // Index into identifier list
-	member      []namespace
+	member      []*namespace
+	parent      *namespace
 }
 
-type literal interface {
-	value() string
+type bytecode interface {
+	bytecode() (marker byte, bytecode string) // All bytecode representations start with a bytecode marker (see above const list)
 }
 
-type function struct {
-	parameter []variable
-	returnVal []variable
-	statement []literal
-}
-
-func (f *function) value() string {
-
-}
-
-type bytes string
-
-func (b bytes) value() string {
-	return b
+type statement interface {
+	bytecode
 }
 
 type variable struct {
-	length uint // Length in bytes
+	*bytelang
+	typ   byte // bByte, bWord, or bBlock
+	block struct {
+		length uint // Length in bytes
+		member []struct {
+			offset uint
+			*namespace
+		}
+	}
 }
 
+func (v *variable) bytecode() (marker byte, bytecode string) {
+	marker = bVariable
+	bytecode = string(v.typ)
+	if v.typ == bBlock {
+		bytecode += v.putWord(v.block.length) + v.putWord(uint(len(v.member)))
+		for _, m := range v.block.member {
+			bytecode += v.putWord(m.offset)
+			bytecode += v.putWord(v.statementIndex(m.variable))
+		}
+	}
+	return
+}
+
+type functionCall struct {
+	*bytelang
+	callee   expression
+	argument []expression
+	receiver []*namespace
+}
+
+func (f *functionCall) bytecode() (marker byte, bytecode string) {
+	marker = bFunctionCall
+	bytecode = f.callee.bytecode()
+	for _, a := range f.argument {
+		bytecode += a.bytecode()
+	}
+	for _, r := range f.receiver {
+		bytecode += f.putWord(f.namespaceIndex(r))
+	}
+}
+
+type expression interface {
+	bytecode
+	value() []byte
+}
+
+type function struct {
+	parameter []*namespace
+	returnVal []*namespace
+	statement []*statement
+}
