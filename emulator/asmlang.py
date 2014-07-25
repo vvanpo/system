2d16e69b495d210aefa843e2e2416bbafed74e3b
Victor van Poppelen <vvanpo@gmail.com>
2014-07-25 17:09:57 -0400

    asmlang.py: pyparsing of the ebnf, need to test

--- emulator/asmlang.py
+++ emulator/asmlang.py
@@ -1,6 +1,27 @@
 
-import pyparsing as pp
+from pyparsing import *
+
+def parse(code):
+	name = Word(alphas)
+	number = Word(nums)
+	address = name + ":" + number |
+				(("_ip" | "_sp" | "_fp") + number)
+	call = "call" + address
+	ref = "ref" + address + number
+	deref = "deref" + address + number
+	literal = "literal" + number
+	operation = ("not" | "and" | "or" | "xor" | "shiftl" | "lshiftr" |
+			"ashiftr" | "add" | "sub" | "mult" | "divfloor" | "exp" | "mod") +
+			number
+	expression = call | ref | deref | literal | operation
+	segment = "segment" + name
+	function = "function" + OneOrMore(Forward(statement)) + "endfunction"
+	pop = "pop" + Optional(number) + Optional(address)
+	statement << (segment | function | pop | ifstmt)
+
+	return statement.parseString(code)
 
 def list_instructions(code):
-	pass
+	p = parse(code)
+	print(p)
 
