b06dff17ec931f59825ff5933599753734a4ed96
Victor van Poppelen <vvanpo@gmail.com>
2014-07-25 12:40:32 -0400

    lang/spec.txt: outline of error handling idea

--- emulator/kernel.py
+++ emulator/kernel.py
@@ -1,4 +1,6 @@
 
+import pyparsing as parse
+
 class fd(list):				# file descriptor
 	def __init__(self, uuid)
 		self.uuid = uuid
@@ -7,6 +9,10 @@ class fd(list):				# file descriptor
 class kernel(object):
 	def __init__(self, init_process):
 		self.fd = {}		# set of files
+		self.process = {}	# set of processes
+	def _exec(self, code):
+		p = process(code)
+		self.process.add(p)
 	def open(self, uuid):
 		for f in self.fd:
 			if f.uuid == uuid:
@@ -15,3 +21,12 @@ class kernel(object):
 		self.fd.add(f)
 		return f
 
+class process(object):
+	def __init__(self, code):
+		def parse_code(c):
+			pass
+		self.instruction = parse_code(code)
+		self.instr_pointer = 0
+	def next(self):
+		self.instr_pointer += 1
+		return self.instruction[self.instr_pointer - 1]
