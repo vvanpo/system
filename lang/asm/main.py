e2a48029327489b386e6b3df3b7aef306732dc10
Victor van Poppelen <vvanpo@gmail.com>
2015-02-17 22:48:45 -0500

    lang/asm/ir/grammar.ebnf: simple intermediate representation grammar

--- lang/asm/main.py
+++ lang/asm/main.py
@@ -1,13 +1,13 @@
 
 # Format of all assembly files is simple:
 #   assembly = 		format, section+
-#     format =		"format", format-name, architecture, newline
-#       format-name =	identifier
-#	architecture =	identifier
+#     format =		"format", format-name, platform, newline
+#       format-name =	identifier (* e.g. elf, coff )
+#       platform =	identifier (* e.g. x86-64_linux, i686_openbsd *)
 #     section =		"section", section-name, newline, 
 #       section-name =	identifier
-#       instruction =	[ label ], 
-#         label = 	identifier, ":"
+#       instruction =	[ label ], ? some string ?
+#         label = 	[ section-name, "." ], identifier, ":"
 #     identifier =	letter, { letter | digit }
 #       letter =	"_" | ? a Unicode code point classified as "Letter" ?
 #       digit =		? a Unicode code point classified as "Decimal Digit" ?
